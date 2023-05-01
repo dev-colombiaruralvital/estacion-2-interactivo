9081bc6724cf9ebce8abc2ca5078912aeb6ef29d
ospersa <ospersa@gmail.com>
2023-05-01 14:24:31 -0500

    Herramienta mensaje de texto

---
+++ assets/js/section-2-1.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('#pagepiling').pagepiling({
+        sectionsColor: ['#ffffff', '#F7F5F6', '#F7F5F6', '#263349'],
+        anchors: ['section-2-1', 'section-2-2', 'section-2-3', 'section-2-4']
+    })
+})
